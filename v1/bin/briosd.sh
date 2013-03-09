fa023be2e25122f2f3a27a5c847542c4296c91d0
Julian Bouzas <nnoell3@gmail.com>
2013-03-09 20:34:13 +0100

    Deleted duplicate files

--- v1/bin/briosd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-brightness=`cat /sys/class/backlight/acpi_video0/actual_brightness`
-brillo=`expr $brightness "*" 100 "/" 9`
-notify-send " " -i notification-display-brightness-low -h int:value:$brillo -h string:x-canonical-private-synchronous:brightness &
